0f1ecc6a99c3b482f3c9104df601a9647ccc7e22
shiyunichi <98327321+shiyunichi@users.noreply.github.com>
2022-02-11 10:21:16 +0900

    発展課題の終了
    Lesson　４の課題です。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,6 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public class Boss
+{
+    private int hp = 100; //体力
+    private int power = 25;　//攻撃力
+    private int mp = 53; //魔力
+
+    //攻撃用の関数
+    public void Attak()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    //防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        //残りhpを減らす
+        this.hp -= damage;
+    }
+
+    //魔法用の関数
+    public void Magic()
+    {
+        if (this.mp > 5)
+        {
+            Debug.Log("魔法攻撃をした");
+        }
+        else
+        {
+            Debug.Log("mpが足りない");
+        }
+
+        //残りmpを減らす
+        this.mp -= 5;
+        
+    }
+
+}
 public class Test : MonoBehaviour
 {
     // Use this for inializaition
@@ -28,7 +67,25 @@ public class Test : MonoBehaviour
         {
             Debug.Log(points[i]);
         }
+
+        //Bossクラスの変数を宣言してインスタンスを代入
+        Boss boss = new Boss();
+
+        //攻撃用の関数を呼び出す
+        boss.Attak();
+        //防御用の関数を呼び出す
+        boss.Defence(3);
+        //魔法用の関数を呼び出す
+        for (int i = 0; i < 11; i++)
+        {
+            boss.Magic();
+        }
+        
+
     }
+
+
+
     // Update is called once per frame
     void Update()
     {
